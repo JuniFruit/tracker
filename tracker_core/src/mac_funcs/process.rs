565c561af4b4acb806fb60ac998268a2cb532bdb
Artem Nebyansky <artemnebyansky@Artems-MacBook-Air.fritz.box>
2023-09-03 20:34:40 +0200

    macOS adapt

--- tracker_core/src/mac_funcs/process.rs
+++ tracker_core/src/mac_funcs/process.rs
@@ -14,31 +14,3 @@ impl ProcessInfo {
         }
     }
 }
-
-pub fn child_stream_to_vec<R>(mut stream: R) -> Vec<u8>
-where
-    R: Read + Send + 'static,
-{
-    let out: Vec<u8> = vec![];
-    let mut vec = out.clone();
-    loop {
-        let mut buf = [0];
-        match stream.read(&mut buf) {
-            Err(err) => {
-                println!("{}] Error reading from stream: {}", line!(), err);
-                break;
-            }
-            Ok(got) => {
-                if got == 0 {
-                    break;
-                } else if got == 1 {
-                    vec.push(buf[0])
-                } else {
-                    println!("{}] Unexpected number of bytes: {}", line!(), got);
-                    break;
-                }
-            }
-        }
-    }
-    out
-}
