6c6a8814c21fe584df94bcb7e41471203b7c3629
Artem Nebyansky <artemnebyansky@Artems-MacBook-Air.fritz.box>
2023-09-08 20:36:20 +0200

    macOS adapt

--- tracker_core/src/lib.rs
+++ tracker_core/src/lib.rs
@@ -1,4 +1,4 @@
-use std::thread;
+use std::{thread, time::Duration};
 
 use crate::store::{apps_store::use_apps_store, user_store::use_user_store};
 use store::{apps_store::Actions, user_store::UserActions};
@@ -15,25 +15,33 @@ extern crate lazy_static;
 pub fn init_data() {
     thread::spawn(move || {
         // Init user related info
-        use_user_store().dispatch(UserActions::InitConfig);
-
-        // fetch prev tracking data
-
-        if use_apps_store().lock().unwrap().selector().is_error_tracked
-            || use_apps_store()
-                .lock()
-                .unwrap()
-                .selector()
-                .tracked_apps
-                .len()
-                > 0
-        {
-            return;
-        }
-        use_apps_store()
+        use_user_store()
             .lock()
             .unwrap()
-            .dispatch(Actions::FetchTrackedApps);
+            .dispatch(UserActions::InitConfig);
+
+        let mut tries: u8 = 0;
+        // fetch prev tracking data
+        loop {
+            if tries > 5
+                || use_apps_store().lock().unwrap().selector().is_error_tracked
+                || use_apps_store()
+                    .lock()
+                    .unwrap()
+                    .selector()
+                    .tracked_apps
+                    .len()
+                    > 0
+            {
+                break;
+            }
+            use_apps_store()
+                .lock()
+                .unwrap()
+                .dispatch(Actions::FetchTrackedApps);
+            thread::sleep(Duration::from_secs(1));
+            tries += 1;
+        }
     });
 
     start_supervisor_thread();
